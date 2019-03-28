d7f41d43af37b21151bccd79a63a3df8742acf32
Jayson Cena <3721620+jaysoncena@users.noreply.github.com>
2019-03-29 05:55:35 +0800

    Add basic onebyte_read() implementation

--- singlebyte/singlebyte.c
+++ singlebyte/singlebyte.c
@@ -48,7 +48,11 @@ ssize_t onebyte_read(struct file *filep, char *buf, size_t count, loff_t *f_pos)
     printk(KERN_ALERT "singlebyte: %s()\n", __FUNCTION__);
     /*please complete the function on your own*/
 
-    return 0;
+    if (copy_to_user(buf, onebyte_data, ONEBYTE_MAXSIZE)) {
+        return -EFAULT;
+    }
+
+    return ONEBYTE_MAXSIZE;
 }
 
 ssize_t onebyte_write(struct file *filep, const char *buf, size_t count, loff_t *f_pos)
